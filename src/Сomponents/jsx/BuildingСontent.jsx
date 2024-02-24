27df898cc3c979f73c4962820ee275c0f13dc04c
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2024-02-24 14:37:05 +0300

    fin

--- src/Сomponents/jsx/BuildingСontent.jsx
+++ src/Сomponents/jsx/BuildingСontent.jsx
@@ -1,9 +1,27 @@
+import "../css/BuildingСontent.css";
+
 export function BuildingСontent(data, setListComponent) {
     if (!Array.isArray(data)) {
-        setListComponent(data.name);
+        setListComponent(
+            <div className="item-cont" key={data.id}>
+                <h1>Температура: {data.main.temp}</h1>
+                <p>Ощущается: {data.main.feels_like}</p>
+                <p>Давление: {data.main.grnd_level}</p>
+                <p>Влажность, %: {data.main.humidity}</p>
+                <p className="item-date">Сейчас</p>
+            </div>
+        );
     }
     if (Array.isArray(data)) {
-        const newData = data.map((item, index) => <div key={index}>{item.dt_txt}</div>)
+        const newData = data.map((item, index) => 
+            <div className="item-cont" key={index}>
+                <h1>Температура: {item.main.temp}</h1>
+                <p>Ощущается: {item.main.feels_like}</p>
+                <p>Давление: {item.main.grnd_level}</p>
+                <p>Влажность, %: {item.main.humidity}</p>
+                <p className="item-date">Дата: {item.dt_txt}</p>
+            </div>
+        )
         setListComponent(newData);
     }
 }
